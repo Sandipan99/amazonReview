79cd5eee71edea37ed3e3220ea9dd853bee08289
Sandipan Sikdar <sandipansikdar@ht-167.humtec.rwth-aachen.de>
2018-08-14 17:48:40 +0200

    commit

--- models/GRU/RNN_model_basic.py
+++ models/GRU/RNN_model_basic.py
@@ -10,6 +10,7 @@ from torch import optim
 import torch.nn.functional as F
 import random
 import preprocess as pp
+import numpy as np
 
 class Encoder(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
@@ -21,8 +22,11 @@ class Encoder(nn.Module):
         self.sigmoid = nn.LogSigmoid()
 
     def forward(self, input, hidden):
-        embedded = self.embedding(input).view(1,1,-1) # view is same as reshape
-        output = embedded
+        print('input shape:',input.size())
+        embedded = self.embedding(input)
+        print('embedding shape:',embedded.size())
+        output = embedded.view(1,1,-1) # view is same as reshape
+        print('embedding view shape:',embedded.size())
         output, hidden = self.gru(output, hidden)
         output = self.sigmoid(self.out(output[0]))
         return output, hidden
@@ -36,7 +40,7 @@ def findTrainExample(train_senetences, train_labels):
 
 
 
-def train(encoder, train_senetences, train_labels, w2i, iter=1000, learning_rate=0.001):
+def train(encoder, train_senetences, train_labels, w2i, iter=1, learning_rate=0.001):
 
     optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
 
@@ -59,6 +63,46 @@ def train(encoder, train_senetences, train_labels, w2i, iter=1000, learning_rate
 
         loss.backward(retain_graph=True)
         optimizer.step()
+
+
+def findBatch(train_sentences, train_labels, batch_size):
+
+    batch_sentences = []
+    batch_labels = []
+    train_size = len(train_labels)
+
+    while batch_size>0:
+        ind = np.random.randint(0,int(train_size))
+        batch_sentences.append(train_sentences[ind])
+        batch_labels.append(train_labels[ind])
+        batch_size-=1
+    return train_sentences,train_labels
+
+
+def batch_train(encoder, train_senetences, train_labels, batch_size, w2i, epochs=5, learning_rate=0.001):
+    optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
+
+    for i in range(1000):
+        batch_sentences, batch_labels = findBatch(train_sentences, train_labels, batch_size)
+        loss = 0
+        encoder_hidden = encoder.initHidden()
+        for j in len(batch_sentences):
+            sentence = batch_sentences[i]
+            label = batch_labels[i]
+            sentence_tensor = pp.encodeSentence(sentence,w2i)
+            label_tensor = torch.tensor(label,dtype=torch.float32).view(1,1)
+
+            input_length = sentence_tensor.size(0)
+            for ei in range(input_length):
+                output, encoder_hidden = encoder(sentence_tensor[ei],encoder_hidden)
+
+            loss += torch.abs(output - label_tensor)
+        loss = loss/len(batch_sentences)
+
+        optimizer.zero_grad()
+        loss.backward(retain_graph=True)
+        optimizer.step()
+
 
 def evaluate(encoder, test_sentences, test_labels, w2i):
 
