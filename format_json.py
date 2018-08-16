d92c431131af90767bec74631e3ebd5dc0cb2cc6
Sandipan Sikdar <sandipansikdar@ht-167.humtec.rwth-aachen.de>
2018-08-16 11:12:46 +0200

    gpu version

--- format_json.py
+++ format_json.py
@@ -1,6 +1,8 @@
 
 import json
+import re
 prop = ['"asin"','"categories"','"price"','"salesRank"','"related"','"buy_after_viewing"','"imUrl"']
+#prop = ['"asin"','"price"','"salesRank"','"related"','"buy_after_viewing"','"imUrl"']
 
 def check(str_):
     for sub_str in prop:
@@ -13,14 +15,20 @@ def removeQuoteBetween(str_):
     e = str_[-1]
     return s+str_[2:-1].replace('"','')+e
 
-with open("json_test") as fs:
+cnt = 0
+
+with open("metadata.json",'rb') as fs:
     for line in fs:
+        cnt+=1
+        line = line.decode()
         line = line.replace('\\','')
         temp = line.strip().split(",")
         flag = 0
         for i in range(len(temp)):
+            print(temp[i],i)
 
             if '"description"' in temp[i]:
+                print ("inside case 1")
                 temp_1 = temp[i].split(":")
                 temp_1[1] = removeQuoteBetween(temp_1[1])
                 temp[i] = temp_1[0]+': '+temp_1[1]
@@ -29,9 +37,10 @@ with open("json_test") as fs:
             if '"title"' in temp[i]:
                 #if temp[i-1][-1]!='"':
                 #    temp[i-1] = temp[i-1]+'"'
+                print("inside case 2")
                 if len(temp[i-1])>0:
                     if not check(temp[i-1]):
-                        if temp[i-1][-1]!='"':
+                        if (temp[i-1][-1]!='"')and(temp[i-1][-1]!='}')and(temp[i-1][-1]!=']'):
                             temp[i-1]=temp[i-1]+'"'
                 else:
                     if not check(temp[i-1]):
@@ -42,18 +51,52 @@ with open("json_test") as fs:
                 flag = 1
                 continue
             if not check(temp[i]) and flag==1:
+                temp[i-1] = temp[i-1][:-2]
+                #print("inside case 3")
                 temp[i] = temp[i].replace('"','')
+                temp[i] = temp[i].replace('[','(')
+                temp[i] = temp[i].replace(']',')')
+                #print(temp[i])
+                #print('-----------------------')
                 continue
             if check(temp[i]):
+                print("inside case 4")
                 if flag==1:
                     if len(temp[i-1])>0:
-                        if temp[i-1][-1]!='"':
+                        print("inside case 4.1")
+                        if (temp[i-1][-1]!='"')and(temp[i-1][-1]!='}')and(temp[i-1][-1]!=']'):
                             temp[i-1]=temp[i-1]+'"'
                     else:
-                        if not check(temp[i-1]):
-                            temp[i-1]+='"'
+                        print("inside case 4.2")
+                        temp[i-1]+='"'
+                        '''
+                        if len(temp[i-1])>0:
+                            if not check(temp[i-1]):
+                                #temp[i-1]+='"'
+                                if (temp[i-1][-1]!='"')and(temp[i-1][-1]!='}')and(temp[i-1][-1]!=']'):
+                                    temp[i-1]=temp[i-1]+'"'
+                        '''
                 flag=0
                 continue
+
+            '''
+            else:
+                print("inside case 5")
+                if len(temp[i])>0:
+                    x = [k for k,v in enumerate(temp[i]) if v=='"']
+                    print("len x: ",len(x))
+                    if len(x)==0:
+                        continue
+                    if len(x)%2==0:
+                        continue
+                    else:
+                        if x[0]==1:
+                            y = [k for k,v in enumerate(temp[i]) if re.match('\w',v)]
+                            temp[i] = temp[i][:1]+temp[i][1:y[-1]+1]+'"'+temp[i][y[-1]+1:]
+                            continue
+            '''
+
+
 
         if flag==1:
             i = len(temp)
@@ -61,10 +104,19 @@ with open("json_test") as fs:
             temp[i-1]+='"}'
         out_string = temp[0]
         for i in range(1,len(temp)):
-            out_string+= ','+temp[i]
+            if len(temp[i])>0:
+                out_string+= ','+temp[i]
 
-        print(out_string)
-        data = json.loads(out_string)
+        #check for contents within square brackets
+                
+
+        try:
+            data = json.loads(out_string)
+        except:
+            print(out_string)
+            print(cnt)
+            print(line)
+            break
         #print(data['description'])
-        print(data['title'])
+        #print(data['title'])
         #print(data['related']['also_viewed'])
---
+++ models/GRU/RNN_model_basic_gpu.py
@@ -0,0 +1,146 @@
+# simple one layer RNN model....
+
+# use preprocess.py for pre-processing tasks..
+
+# improve on this
+
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+import random
+import preprocess as pp
+import numpy as np
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class Encoder(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(Encoder, self).__init__()
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.sigmoid = nn.LogSigmoid()
+
+    def forward(self, input, hidden):
+        for ei in range(input.size(0)):
+            embedded = self.embedding(input[ei])
+            output = embedded.view(1,1,-1) # view is same as reshape
+            output, hidden = self.gru(output, hidden)
+            output = self.sigmoid(self.out(output[0]))
+        return output
+
+    def initHidden(self):
+         return torch.zeros(1, 1, self.hidden_size)
+
+def findTrainExample(train_senetences, train_labels):
+    ind = random.randint(0,len(train_senetences)-1)
+    return train_senetences[ind], train_labels[ind]
+
+
+
+def train(encoder, train_senetences, train_labels, w2i, iter=1, learning_rate=0.001):
+
+    optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
+
+    for i in range(iter):
+        sentence, label = findTrainExample(train_senetences, train_labels)
+        sentence_tensor = pp.encodeSentence(sentence,w2i)
+        label_tensor = torch.tensor(label,dtype=torch.float32).view(1,1)
+
+        encoder_hidden = encoder.initHidden()
+
+        input_length = sentence_tensor.size(0)
+        for ei in range(input_length):
+            output, encoder_hidden = encoder(sentence_tensor[ei],encoder_hidden)
+
+        loss = torch.abs(output - label_tensor)
+        if i%100==0:
+            print("loss: ",loss)
+
+        optimizer.zero_grad()
+
+        loss.backward(retain_graph=True)
+        optimizer.step()
+
+
+def findBatch(train_sentences, train_labels, batch_size):
+
+    batch_sentences = []
+    batch_labels = []
+    train_size = len(train_labels)
+
+    while batch_size>0:
+        ind = np.random.randint(0,int(train_size))
+        batch_sentences.append(train_sentences[ind])
+        batch_labels.append(train_labels[ind])
+        batch_size-=1
+    return batch_sentences,batch_labels
+
+
+def batch_train(encoder, train_sentences, train_labels, batch_size, w2i, epochs=5, learning_rate=0.001):
+    optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
+
+    for i in range(1000):
+        batch_sentences, batch_labels = findBatch(train_sentences, train_labels, batch_size)
+        print('found batch')
+        loss = 0
+        encoder_hidden = encoder.initHidden()
+        for j in range(len(batch_sentences)):
+            sentence = batch_sentences[j]
+            label = batch_labels[j]
+            sentence_tensor = pp.encodeSentence(sentence,w2i)
+            label_tensor = torch.tensor(label,dtype=torch.float32).view(1,1)
+
+            input_length = sentence_tensor.size(0)
+            #for ei in range(input_length):
+            sentence_tensor = sentence_tensor.to(device)
+            label_tensor = label_tensor.to(device)
+            output = encoder(sentence_tensor,encoder_hidden)
+
+            loss += torch.abs(output - label_tensor)
+            #print(loss)
+        loss = loss/len(batch_sentences)
+        print(loss)
+
+        optimizer.zero_grad()
+        loss.backward(retain_graph=True)
+        optimizer.step()
+
+
+def evaluate(encoder, test_sentences, test_labels, w2i):
+
+    accuracy = 0.0
+
+    for i in range(len(test_sentences)):
+        sentence = test_sentences[i]
+        label = test_labels[i]
+        sentence_tensor = pp.encodeSentence(sentence,w2i)
+        label_tensor = torch.tensor(label,dtype=torch.float32).view(1,1)
+
+        encoder_hidden = encoder.initHidden()
+
+        input_length = sentence_tensor.size(0)
+        for ei in range(input_length):
+            output, encoder_hidden = encoder(sentence_tensor[ei],encoder_hidden)
+
+        output = torch.round(output)
+        if torch.equal(output,label_tensor):
+            accuracy+=1
+
+    return accuracy/len(test_sentences)
+
+if __name__=='__main__':
+
+    # file name for male reviews and female reviews
+    vocabulary,w2i,sentences_m,sentences_f = pp.obtainW2i("../Data/sample_male","../Data/sample_female")
+    train_senetences, train_labels, test_sentences, test_labels = pp.testTrainSplit(sentences_m, sentences_f)
+    hidden_size = 20
+    input_size = len(w2i)
+    output_size = 1
+    encoder = Encoder(input_size, hidden_size, output_size)
+    encoder = encoder.to(device)
+    batch_train(encoder, train_senetences, train_labels, 3, w2i)
+    #accuracy = evaluate(encoder,test_sentences, test_labels,w2i)
+    #print(accuracy)
